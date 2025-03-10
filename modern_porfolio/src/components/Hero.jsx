5a552422d094ddd127fbc3942480af0496edc303
kengnidoriane <dorianekengni@gmail.com>
2025-03-10 13:49:32 +0100

    add credentials on hero and header sections

--- modern_porfolio/src/components/Hero.jsx
+++ modern_porfolio/src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
     <section id="home" className="min-h-screen flex items-center bg-gradient-to-b from-blue-50 to-white pt-20">
     <div className="container mx-auto px-6">
       <div className="max-w-3xl">
-        <p>Hello I'm</p>
+        <p className="text-2xl md:text-4xl text-gray-900 mb-2">Hello I'm</p>
         <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6">
           Kengni Doriane
         </h1>
-        <h4 className="text-xl md:text-6xl font-bold text-gray-800 mb-6">
+        <h4 className="text-xl md:text-5xl font-bold text-blue-600 mb-6">
           <TypewriterText text="Web Developer" />
         </h4>
         <p className="text-xl text-gray-600 mb-8">
